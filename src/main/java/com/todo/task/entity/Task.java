1e26f2bae91e4aefc1a5829f26ff0f61ca628770
davit412v2 <luisdavidbarrigagaray97@gmail.com>
2026-05-13 18:18:06 -0500

    Add Task and User entity classes with Lombok annotations and additional fields

---
+++ src/main/java/com/todo/task/entity/Task.java
@@ -0,0 +1,40 @@
+package com.todo.task.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
